ea119f6e8e42f8cd1b378603da82c060442dd67f
Krishnendu Bhattacharyya <krishnendu19802@gmail.com>
2024-06-21 14:01:02 +0530

    fixed UI

--- frontend/src/Components/ImageUpload.js
+++ frontend/src/Components/ImageUpload.js
@@ -9,7 +9,7 @@ const ImageUpload = () => {
     const [personImageSrc, setPersonImageSrc] = useState('');
     const [fabricImageSrc, setFabricImageSrc] = useState('');
     const [loading, setLoading] = useState(false)
-    const [status,setStatus]=useState([false])
+    const [status, setStatus] = useState([false])
 
     const [imageSrc, setImageSrc] = useState('');
 
@@ -32,10 +32,10 @@ const ImageUpload = () => {
         try {
             if (personFile === null || fabricFile === null) {
                 console.log('Please select both files');
-                setStatus([true,'Please select both the files'])
+                setStatus([true, 'Please select both the files'])
                 return;
             }
-            if(status[0])
+            if (status[0])
                 setStatus([false])
             setLoading(true)
             const formData = new FormData();
@@ -53,7 +53,7 @@ const ImageUpload = () => {
             const imageUrl = URL.createObjectURL(response.data);
             setLoading(false)
             setImageSrc(imageUrl);
-            
+
 
         } catch (error) {
             console.error('Error uploading image', error);
@@ -62,17 +62,38 @@ const ImageUpload = () => {
     };
 
     return (
-        <div className={`max-w-xs mx-auto flex flex-col justify-center min-h-screen items-center ${imageSrc!==''?'mt-8':''} p-4 md:p-0`}>
-            <div className="image-form p-8 shadow-xl rounded-md flex flex-col justify-center">
-                <div className="flex flex-col md:flex-row  w-full md:justify-between my-4 items-center">
-                    <input type="file" name='person' onChange={handleFileChange} className="mb-4 ml-16 md:ml-0" />
+        <div className={` mx-auto flex flex-col w-full justify-center min-h-screen items-center ${imageSrc !== '' ? 'mt-8' : ''} p-4 md:p-0`}>
+            <div className="image-form p-8 shadow-xl w-4/5 md:w-1/3 rounded-md flex flex-col justify-center">
+                
+            <div className="flex flex-col md:flex-row w-full md:justify-between my-4 items-center">
+                    <div className="flex flex-col items-center md:items-center md:mr-4">
+                        <label className="block mb-2 text-start text-sm font-medium text-gray-700" htmlFor="person">
+                            Upload Your Image
+                        </label>
+                        <input
+                            type="file"
+                            name="person"
+                            id="person"
+                            onChange={handleFileChange}
+                            className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer focus:outline-none focus:border-blue-500 p-2"
+                        />
+                    </div>
                     {personImageSrc && <ImageShow source={personImageSrc} altText={'Original Image'} size={12} />}
-
                 </div>
 
-                <div className="flex flex-col md:flex-row justify-between p-2  my-4 items-center">
-                    <input type="file" name='fabric' onChange={handleFileChange} className="mb-4 ml-16 md:ml-0" />
-
+                <div className="flex flex-col md:flex-row w-full md:justify-between my-4 items-center">
+                    <div className="flex flex-col items-center md:items-center md:mr-4">
+                        <label className="block mb-2 text-start text-sm font-medium text-gray-700" htmlFor="fabric">
+                            Upload Fabric Image
+                        </label>
+                        <input
+                            type="file"
+                            name="fabric"
+                            id="fabric"
+                            onChange={handleFileChange}
+                            className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer focus:outline-none focus:border-blue-500 p-2"
+                        />
+                    </div>
                     {fabricImageSrc && <ImageShow source={fabricImageSrc} altText={'Original Image'} size={12} />}
                 </div>
 
@@ -103,13 +124,13 @@ const ImageUpload = () => {
 
             </div>}
 
-            {loading &&<div className="p-4 flex justify-center">
+            {loading && <div className="p-4 flex justify-center">
                 <Loader size={96} />
 
             </div>}
 
 
-            
+
         </div>
     );
 };
